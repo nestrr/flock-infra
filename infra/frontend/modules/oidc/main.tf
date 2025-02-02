df1ddc47585833231963c744198a546c77570962
Maryam Khan <maryam.dev@outlook.com>
2025-02-01 16:48:55 -0800

    Fix OIDC module's allowed branches, disable by default (#4)

--- infra/frontend/modules/oidc/main.tf
+++ infra/frontend/modules/oidc/main.tf
@@ -1,9 +1,13 @@
+data "aws_caller_identity" "self" {}
+
 module "oidc_github" {
   source              = "git::https://github.com/unfunco/terraform-aws-oidc-github.git?ref=f664e8f6002b11b5c206f1fb3cf0377ea6a033ae"
   github_repositories = var.allowed_repos_branches
   iam_role_inline_policies = {
     "tg_policy" : data.aws_iam_policy_document.iam_policy.json
   }
+  enabled = var.enable_resource_creation
+  create_oidc_provider = var.enable_oidc_provider_creation
 }
 
 data "aws_iam_policy_document" "iam_policy" {
--- infra/frontend/modules/oidc/variables.tf
+++ infra/frontend/modules/oidc/variables.tf
@@ -1,5 +1,15 @@
 variable "allowed_repos_branches" {
   description = "GitHub repos/branches allowed to assume the IAM role."
   type        = list(string)
-  default     = ["nestrr/flock-infra:ref:refs/heads/main"]
+  default     = ["nestrr/flock-infra"]
+}
+variable "enable_resource_creation" {
+  description = "Whether the oidc module is allowed to create resources"
+  type = bool
+  default = false
+}
+variable "enable_oidc_provider_creation" {
+  description = "Whether the oidc module is allowed to create provider"
+  type = bool
+  default = false
 }
